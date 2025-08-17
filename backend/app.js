2fecf835edf679f6c1e5e3a4e27acbcb6a2264ee
Navneet <navneetkumar142000@gmail.com>
2025-08-17 13:05:19 +0530

    feat: serving frontend from server

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const searchRoutes = require('./routes/searchRoutes');
 const connectDB = require('./config/db');
+const path = require("path");
 
 const app = express();
 const port = 5000;
@@ -16,6 +17,15 @@ connectDB();
 
 // Routes
 app.use('/api/v1', searchRoutes);
+
+//Express backend serving React build files
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
 
 // Start the server
 app.listen(port, () => {
